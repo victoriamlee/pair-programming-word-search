6d3a53361b6ccf3b6b91d241407c1ef4e3dd77e5
Victoria Lee <victorialee72@live.com>
2020-07-29 21:57:32 +0000

    Refactored code, paired with Tania

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+    let output = [];
+    let numRows = matrix[0].length;
+    let numColumns = matrix.length;
+    for (let i = 0; i < numRows; i++) {
+      let arr = [];
+      for (let j = 0; j < numColumns; j++) {
+        arr.push(matrix[j][i]);
+      } output.push(arr);
     }
+    return output;
+  };
+
+const wordSearch = (letters, word) => { 
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        } else {
+            const verticalJoin = transpose(letters).map(ls => ls.join(''));
+            for (l of verticalJoin) {
+                if (l.includes(word)) {
+                    return true;
+                }
+            }
+        }
+    } return false;
 }
 
 module.exports = wordSearch
